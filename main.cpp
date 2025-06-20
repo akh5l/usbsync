da66732c4ed1f9635509eec549748e68395e44b9
akh5l <xdteddypro@gmail.com>
2025-06-20 03:54:23 +0100

    Added USB auto-detect. Stores UUID in conf file

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@
 #include <vector>
 #include <fnmatch.h>
 
+#define DEBUG 0 // set to 0 to disable debug output
+
 namespace fs = std::filesystem;
 
-int mountUSB(bool arg) { // true for mount, false for unmount
+int mountUSB(bool mount, std::string UUID) { // true for mount, false for unmount
   int result;
-  if (arg) {
+  if (mount) {
     std::cout << "Mounting USB drive..." << std::endl;
-    // change /dev/sdc1 so that it can find the desired drive and save it in some config file
 
-    const char* cmd = "sudo mount /dev/sdc1 /mnt/usbbackup";
-    result = std::system(cmd);
+    // const char* cmd = ("sudo mount -U " + UUID + " /mnt/usbbackup").c_str();
+    result = std::system(("sudo mount -U " + UUID + " /mnt/usbbackup").c_str());
   }
   else {
     std::cout << "Unmounting USB drive..." << std::endl;
@@ -52,7 +53,7 @@ int copyToUSB(const fs::path& source, const fs::path& destination, const std::ve
 
       if (!fs::exists(destinationPath) || fs::last_write_time(entry.path()) > fs::last_write_time(destinationPath)) {
         fs::copy_file(entry.path(), destinationPath, fs::copy_options::overwrite_existing);
-         std::cout << "Copied: " << entry.path() << " to " << destinationPath << '\n';
+        //  std::cout << "Copied: " << entry.path() << " to " << destinationPath << '\n';
          filesCopied++;
       }
     }
@@ -73,8 +74,121 @@ int copyToUSB(const fs::path& source, const fs::path& destination, const std::ve
   }
 }
 
+int chooseUSB() {
+  FILE* pipe = popen("lsblk -o VENDOR,MODEL,NAME | grep USB | awk '{print $NF}'", "r");
+  if (!pipe) {
+    perror("popen failed");    
+    return 1;
+  }
+
+
+  std::vector<std::string> devices;
+  std::string buffer; // buffer holds output
+
+  char temp[256];
+  while (fgets(temp, sizeof(temp), pipe) != nullptr) {
+    buffer = temp;
+    size_t len = buffer.length();
+    if (len > 0 && buffer[len-1] == '\n') {
+        buffer.erase(len-1, 1);
+    }
+
+    devices.push_back(buffer);
+    
+  }
+
+  pclose(pipe);
+
+  std::string selectedDevice;
+
+  if (devices.empty()) {
+    std::cerr << "No USB devices found." << std::endl;
+    return 1;
+  } else if (devices.size() == 1) {
+
+    std::cout << "One USB device found: " << std::endl;
+    std::cout << '\n';
+    std::system("lsblk -o MODEL,VENDOR,NAME | grep USB");
+    std::cout << '\n';
+
+    selectedDevice = devices[0];
+  } else if (devices.size() > 1) {
+    int choice = 0;
+    std::cout << "Multiple USB devices found: " << '\n' << std::endl;
+    std::system("lsblk -o MODEL,VENDOR,NAME | grep USB");
+    std::cout << '\n' << "Please choose a USB device." << std::endl;
+    std::cout<< "(Pick a number from 1 to " << devices.size() << "): ";
+
+    while (choice < 1 || choice > devices.size()) {
+      std::cin >> choice;
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << std::endl;
+    }
+    selectedDevice = devices[choice - 1];
+    
+  }
+  
+  devices.clear();
+
+  pipe = popen(("lsblk -o NAME,UUID | grep -E " + selectedDevice + "[0-9]+" + " | awk '{print $NF}'").c_str(), "r");
+  if (!pipe) {
+    perror("popen failed");
+    return 1;
+  }
+
+  while (fgets(temp, sizeof(temp), pipe) != nullptr) {
+    buffer = temp;
+    size_t len = buffer.length();
+    if (len > 0 && buffer[len-1] == '\n') {
+        buffer.erase(len-1, 1);
+    }
+
+    devices.push_back(buffer);
+  }
+
+  pclose(pipe);
+
+  if (devices.empty()) { // TODO handle case where there's 1 partition
+    std::cerr << "No partitions found for the selected USB device." << std::endl;
+    return 1;
+  } else if (devices.size() == 1) {
+    std::cout << "One partition found: " << devices[0] << std::endl;
+    std::cout << '\n';
+  } else if (devices.size() > 1) {
+    int choice = 0;
+
+    std::cout << "Multiple partitions found: " << '\n';
+    std::system(("lsblk -o NAME,FSTYPE,SIZE | grep -E " + selectedDevice + "[0-9]+").c_str());
+    std::cout << '\n';
+
+    std::cout << "Please choose a partition." << std::endl;
+    std::cout << "(Pick a number from 1 to " << devices.size() << "): ";
+
+    while (choice < 1 || choice > devices.size()) {
+      std::cin >> choice;
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << std::endl;
+    }
+    selectedDevice = devices[choice - 1];
+  }
+
+  devices.clear();
+
+  std::ofstream configFile("usbsync.conf", std::ios::app);
+
+  configFile << selectedDevice << std::endl;
+  configFile.close();
+  
+  std::cout << "USB device saved to configuration file." << std::endl;
+
+  return 0;
+}
+
 void generateConfigFile() {
-  if (fs::exists("usbsync.conf")) return;
+
+  if (fs::exists("usbsync.conf")) {
+    return;
+  }
 
   std::ofstream configFile("usbsync.conf");
   std::cout << "Generating configuration file usbsync.conf..." << std::endl;
@@ -101,7 +215,12 @@ void generateConfigFile() {
 
   std::cout << std::endl;
 
-  std::cout << "Configuration file usbsync.conf created." << std::endl;
+  if (chooseUSB() != 0) {
+    std::cerr << "Error: Failed to choose USB device." << std::endl;
+    return;
+  }
+
+  std::cout << "Configuration file created." << std::endl;
   configFile.close();
 }
 
@@ -124,27 +243,33 @@ std::vector<fs::path> readConfigFile() {
 }
 
 int main() {
+
   generateConfigFile();
+
   std::vector<fs::path> excludePaths = readConfigFile();
-
   fs::path documentsPath = excludePaths.front();
+
   excludePaths.erase(excludePaths.begin());
+  std::string usbUUID = excludePaths.back();
+  excludePaths.pop_back();
 
 
-  if (mountUSB(true) != 0) {
+  if (DEBUG) return 0; // debug
+
+  if (mountUSB(true, usbUUID) != 0) {
     std::cerr << "Error: Failed to mount USB drive." << std::endl;
     return 1;
   }
 
-  fs::path usbPath = "/mnt/usbbackup/Backup"; // more robust solution?
+  fs::path usbPath = "/mnt/usbbackup/USBSync";
   
   if (copyToUSB(documentsPath, usbPath, excludePaths) != 0) {
     std::cerr << "Error: Failed to copy files to USB drive. " << std::endl;
-    mountUSB(false);
+    mountUSB(false, usbUUID);
     return 1;
   }
 
-  mountUSB(false);
+  mountUSB(false, usbUUID);
   std::cout << "Backup completed successfully." << std::endl;
   return 0;
 }
